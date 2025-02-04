7adc33f258ab7d4b60e54593e783edcccc66fd7a
melvzz1204 <137977788+melvzz1204@users.noreply.github.com>
2025-02-04 10:32:46 +0800

    minimal

--- src/main.js
+++ src/main.js
@@ -1,25 +1,19 @@
+import "../style css/modern-normalize.css";
+import "../style css/style.css";
+import "../style css/Header.css";
+import "../style css/hero.css";
+import "../style css/about.css";
+import "../style css/featured.css";
+import "../style css/work.css";
+import "../style css/contact.css";
+import "../style css/footer.css";
+import "../style css/mobile-nav.css";
+import "../style css/util.css";
 
-import '../style css/modern-normalize.css';
-import '../style css/style.css';
-import '../style css/Header.css';
-import '../style css/hero.css';
-import '../style css/about.css'
-import '../style css/featured.css';
-import '../style css/work.css';
-import '../style css/contact.css';
-import '../style css/footer.css';
-import '../style css/mobile-nav.css';
-import '../style css/util.css';
-
-
-import mobilenav from './util/mobile-nav.js';
-import darkMode from './util/dark-mode.js';
-import lazyimg from './util/lazy-image.js';
+import mobilenav from "./util/mobile-nav.js";
+import darkMode from "./util/dark-mode.js";
+import lazyimg from "./util/lazy-image.js";
 
 mobilenav();
 darkMode();
-lazyimg();  
-
-
-
-
+lazyimg();
--- src/util/mobile-nav.js
+++ src/util/mobile-nav.js
@@ -1,38 +1,27 @@
 const mobileNav = () => {
+  const headerBtn = document.querySelector(".header__bars");
+  const mobileNav = document.querySelector(".mobile-nav");
+  const mobileLink = document.querySelectorAll(".mobile-nav__list");
 
+  let mobileNavOpen = "false";
 
-    const headerBtn = document.querySelector('.header__bars');
-    const mobileNav = document.querySelector('.mobile-nav');
-    const mobileLink = document.querySelectorAll ('.mobile-nav__list');
-
-    let mobileNavOpen = 'false';
-    
-    headerBtn.addEventListener('click', () => {
+  headerBtn.addEventListener("click", () => {
     mobileNavOpen = !mobileNavOpen;
     if (mobileNavOpen) {
-
-        mobileNav.style.display = 'flex';
-        document.body.style.overflowY = 'hidden';
-        
+      mobileNav.style.display = "flex";
+      document.body.style.overflowY = "hidden";
+    } else {
+      mobileNav.style.display = "none";
+      document.body.style.overflowY = "auto";
     }
-    else {
+  });
 
-        mobileNav.style.display = 'none';
-        document.body.style.overflowY = 'auto';
-        
-    }
+  mobileLink.forEach((Link) => {
+    Link.addEventListener("click", () => {
+      mobileNavOpen = "false";
+      mobileNav.style.display = "none";
+      document.body.style.overflowY = "";
     });
-
-    mobileLink.forEach (Link =>{
-
-        Link.addEventListener ('click', () => {
-            mobileNavOpen = 'false';
-            mobileNav.style.display = 'none';
-            document.body.style.overflowY = 'auto';
-        });
-
-    }); 
-
+  });
 };
-export default mobileNav;    
-
+export default mobileNav;
